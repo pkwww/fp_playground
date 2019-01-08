b53952ea7df99e4106b896e8c29bc03abbed1221
pkwww <pkwww.2000@gmail.com>
2019-01-08 14:48:19 +0000

    monad def and example

--- monadic_parser.hs
+++ monadic_parser.hs
@@ -1,45 +1,83 @@
-import Control.Monad (liftM, ap)
+import Control.Monad
+import Control.Applicative
 
 data Parser a = P (String -> [(a, String)])
 
 instance Functor Parser where
   fmap = liftM
 instance Applicative Parser where
-  pure = return
+  pure = result
   (<*>) = ap
 instance Monad Parser where
-  -- return :: a -> Parser a
-  return v = P $ \inp -> [(v, inp)]
-  p >>= f = P $ \inp -> concat [parse (f v) inp' | (v, inp') <- parse p inp]
-
-item :: Parser Char
-item = P $ \inp -> case inp of
-                [] -> []
-                (x:xs) -> [(x, xs)]
-
-failure :: Parser a
-failure = P $ \_ -> []
+  return = pure
+  (>>=) = bind
+instance Alternative Parser where
+  empty = failure
+  (<|>) = choice
+instance MonadPlus Parser where
+  mzero = empty
+  mplus = (<|>)
 
 parse :: Parser a -> String -> [(a, String)]
 parse (P p) inp = p inp
 
+result :: a -> Parser a
+result v = P $ \inp -> [(v, inp)]
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+p `bind` f = P $ \inp -> concat [parse (f v) inp' | (v, inp') <- parse p inp]
+
+failure :: Parser a
+failure = P $ \_ -> []
+
+choice :: Parser a -> Parser a -> Parser a
+p `choice` q = P $ \inp -> parse p inp ++ parse q inp
+
 (+++) :: Parser a -> Parser a -> Parser a
-p +++ q = P $ \inp -> case parse p inp of
-                    [] -> parse q inp
-                    [(v, s)] -> [(v, s)]
+p +++ q = P $ \inp -> case parse (p <|> q) inp of
+                        [] -> []
+                        (x:xs) -> [x]
 
 seq :: Parser a -> Parser b -> Parser (a, b)
-p `seq` q = p >>= \x ->
-            q >>= \y ->
-            return (x, y)
+p `seq` q = do { x <- p
+               ; y <- q
+               ; return (x, y) }
 
-seq' :: Parser a -> Parser b -> Parser (a, b)
-p `seq'` q = do { x <- p
-                ; y <- q
-                ; return (x, y) }
+item :: Parser Char
+item = P $ \inp -> case inp of
+                    [] -> []
+                    (x:xs) -> [(x, xs)]
 
--- first_third :: Parser (Char, Char)
--- first_third = do x <- item
---                  item
---                  y <- item
---                  returnP (x, y)
+sat :: Parser a -> (a -> Bool) -> Parser a
+sat parser predicate = do { x <- parser
+                          ; if predicate x 
+                            then return x 
+                            else mzero
+                          }
+
+satC :: (Char -> Bool) -> Parser Char
+satC = sat item
+
+char :: Char -> Parser Char
+char c = satC (c ==)
+
+digit :: Parser Char
+digit = satC (\d -> d >= '0' && d <= '9')
+
+lower :: Parser Char
+lower = satC (\c -> c >= 'a' && c <= 'z')
+
+upper :: Parser Char
+upper = satC (\c -> c >= 'A' && c <= 'Z')
+
+letter :: Parser Char
+letter = lower <|> upper
+
+alphanum :: Parser Char
+alphanum = digit <|> letter
+
+first_third :: Parser (Char, Char)
+first_third = do { x <- item
+                 ; _ <- item
+                 ; y <- item
+                 ; return (x, y) }
